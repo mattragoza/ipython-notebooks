42b1f5f2394427d05ec3b362c424c6234dfd7fed
Matt Ragoza <mtr22@pitt.edu>
2020-05-05 22:13:01 -0400

    untrack jupyter_nb.sh

--- jupyter_nb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=jupyter_nb
-#SBATCH -p GPU-AI
-#SBATCH --gres=gpu:volta16:1
-#SBATCH -t 12:00:00
-#SBATCH --dependency=singleton
-jupyter notebook --no-browser --ip=0.0.0.0
-echo done
-
